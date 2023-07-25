843b861e7ee27578a56c149cc41dbc0a0ffc417a
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-07-26 00:55:51 +0300

    mobile menu

--- src/Header/HeaderMobile.jsx
+++ src/Header/HeaderMobile.jsx
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { IoPaw } from "react-icons/io5";
+import { css, styled } from "styled-components";
 
 function HeaderMobile() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  function handleClick(event) {
+    if (event.target.tagName === "BUTTON") {
+      setMobileMenuOpen(!mobileMenuOpen);
+    }
+  }
   return (
-    <header>
+    <Header>
       <div>
         <div>
           <IoPaw />
         </div>
-        <button>Menu</button>
       </div>
-      <div>
-        <nav>
-          <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
+      <button onClick={handleClick}>Menu</button>
+      {mobileMenuOpen === true && (
+        <MobileMenu isOpen={mobileMenuOpen}>
+          <button onClick={handleClick}>Close</button>
+          <nav>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
+        </MobileMenu>
+      )}
+    </Header>
   );
 }
 
 export default HeaderMobile;
+
+const Header = styled.header`
+  position: relative;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+
+const MobileMenu = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: pink;
+  top: 0;
+  left: 0;
+  animation-duration: 1s;
+  animation-name: menuOpen;
+
+  @keyframes menuOpen {
+    from {
+      margin-left: 100%;
+    }
+
+    to {
+      margin-left: 0%;
+    }
+  }
+`;
